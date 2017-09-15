9b0f23bbf57d166406512f5ff19662af78f4cd30
Brian Ketterman <brian.ketterman@gmail.com>
2017-09-15 14:49:26 -0400

    Add project files.

---
+++ HelloGit4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloGit4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(); 
+        }
+    }
+}
